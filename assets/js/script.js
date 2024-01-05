eb3e7f2cb0d2d070477b5707d147c17c72664724
12kharris <>
2024-01-05 18:02:34 +0000

    add event listeners to buttons with placeholder functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,23 @@
+// Wait for DOM to finish loading before running the game
+// Get button elements and add event listeners to them
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!");
+            }
+            else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}!`);
+            }
+        })
+    }
+})
 
 function runGame() {
-    
+
 }
 
 function checkAnswer () {
@@ -28,5 +45,5 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
