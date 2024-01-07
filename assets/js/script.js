3483df6311bf05abe179d44c18ab899a348c8446
12kharris <>
2024-01-07 09:30:22 +0000

    Add QOL improvements for questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
 });
 
@@ -23,6 +29,10 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+    let answerBox = document.getElementById("answer-box");
+    answerBox.value = "";
+    answerBox.focus();
+
     let num1 = Math.ceil(Math.random() * 25);
     let num2 = Math.ceil(Math.random() * 25);
 
